23c86e08734461107caadbd10b5eeefee3cb8b8d
kanwararpit <arpit.kanwar@quarkifi.com>
2025-01-11 16:24:30 +0530

    fourth commit

--- helpers/getDataFromToken.ts
+++ helpers/getDataFromToken.ts
@@ -4,6 +4,12 @@ import jwt from 'jsonwebtoken'
 export const getDataFromToken = async (request: NextRequest) => {
   try {
     const token = (await request.cookies.get('token')?.value) || ''
+
+    // Ensure that JWT_SECRET is defined
+    if (!process.env.JWT_SECRET) {
+      throw new Error('JWT_SECRET environment variable is not defined')
+    }
+
     const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET)
 
     // Return decodedToken.id directly, no need to return an object
